746ad235d22516ae1c300010e3e67aeb9461d5c2
mknjbhvh <camila1034_@hotmail.com>
2024-01-22 00:05:27 -0500

    trabajo terminado con comentarios adjuntos

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-require('colors')
+require('colors')//Se importa el módulo 'colors' para agregar color
 
-const main= async() =>{
-    console.clear();
-    let nombres= ('Maria Camila');
+const main= async() =>{//Se declara la función main con la palabra clave async, lo que significa que puede contener operaciones asíncronas
+    console.clear();//Se utiliza console.clear() para limpiar la consola
+    let nombres= ('Maria Camila');//Se definen las variables nombres y apellidos 
     let apellidos= ('Bernal');
     let nombrecompleto = nombres[0].toUpperCase() + nombres.substring(1).toLocaleLowerCase() + ' ' + apellidos[0].toUpperCase()+apellidos.substring(1).toLocaleLowerCase();
+    //e crea la variable nombrecompleto uniendo las partes de los nombres y apellidos con formato de mayúscula inicial y minúsculas para el resto.
     console.log('::::::::::::::::::::::::::'.bgGreen);
     console.log(':::'.bgGreen + 'NOMBRE ORGANIZADO'.bgBlack + '::::::'.bgGreen);
     console.log('::::::::::::::::::::::::::'.bgGreen)
@@ -15,6 +16,7 @@ const main= async() =>{
     for(let i = 1; i<= nombrecompleto.length; i++){
         console.log (':::'.bgBlue + i + ' '.bgBlack + nombrecompleto.substring(0, i).bgBlack + ':::'.bgBlue);
     }
+    //Se utilizan dos bucles for para imprimir el nombre organizado
     
     console.log(':::::::::::::::::::::::::'.bgGreen);
     console.log(':::'.bgGreen + nombres.toUpperCase().bgCyan+' '.bgCyan+apellidos.toUpperCase().bgCyan + ':::'.bgGreen);
@@ -23,4 +25,4 @@ const main= async() =>{
     console.log(':::::::::::::::::::::::::'.bgGreen);
 }
 
-main();
+main();//se cierra main para ejecutar lo que hay en ella
